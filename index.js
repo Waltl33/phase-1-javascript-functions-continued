7ecf541e4cef95200d7f60bd296562cb7810ee5a
Walter <walter.clark24@yahoo.com>
2022-12-12 16:54:59 -0500

    commit tree f0ef29f2b99303dc68ead638e800e3a404f0aacf

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function saturdayFun (activity ='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun()
+
+function mondayWork( job = "go to the office"){
+    return `This Monday, I will ${job}.`
+}
